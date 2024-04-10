e52cd249bc6361816162cd8eefb2e72e8d724050
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-10 11:22:13 -0500

    Added leaflet.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // Add all scripts to the JS folder
+
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
