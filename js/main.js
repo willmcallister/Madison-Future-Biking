50795674679f31466ab4c9edc6bae063a74f2425
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-19 11:13:33 -0500

    Recentered map on Madison

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Add all scripts to the JS folder
 
-var map = L.map('map');
+var map = L.map('map').setView([43.07, -89.4], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
