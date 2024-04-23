f900cf14cdc303aecd73c49579bd979e465d8f64
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-23 13:21:50 -0500

    Changed tile layer, fit map to Madison with bounded pan and zoom

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 // Add all scripts to the JS folder
 
-var map = L.map('map').setView([43.07, -89.4], 10);
+var map; 
 
+/*
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+*/
+
+function createMap(){
+
+    map = L.map('map').setView([43.07, -89.4], 13);
+
+    // set max bounds for the map -- MAY WANT TO ADJUST LATER
+    map.setMaxBounds(map.getBounds().pad(1));
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        minZoom: 11
+    }).addTo(map);
+
+};
+
+document.addEventListener('DOMContentLoaded',createMap);
